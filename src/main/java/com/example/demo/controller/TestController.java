5eb717e664db97e4e2d767e3e74e9fb056be9d14
yushuonh <yushuonh@163.com>
2018-09-11 23:27:04 +0800

    new  project

---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author yushuo
+ * @date 2018/9/11 23:22
+ */
+@RestController
+public class TestController {
+
+    @RequestMapping("/uu")
+    public String say(){
+        return "yushuo";
+    }
+
+}
